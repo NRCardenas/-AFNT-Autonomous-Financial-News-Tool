6a069a7d1f7a03177f08117c8f98d8c732838375
NRCardenas <153119544+NRCardenas@users.noreply.github.com>
2023-12-13 23:34:44 +0100

    Create FAKE.py
    Fake file to give a description of the private functions that functions as main functions to elaborate the financial reports

---
+++ FAKE.py
@@ -0,0 +1,31 @@
+"""
+This file is created to give a brief description of the whole program fuctionality
+
+A diagram can be found at the diagram.png file
+"""
+
+def news_search():
+    """
+    This function returns a list of news that are useful to gain current market knowledge
+    :return: List of news that are important for the current and selected market
+    """
+
+def AI_report(descripciones):
+    """
+    This function call a pre-trained LLM that takes the news as input. With this input elaborates a
+    detailed report for the current market and economic situation
+
+    The LLM is trained with expert like financial reports so the AI model has experience creating this kind
+    of reports and can simulate high human skill levels.
+    :param descripciones: This parameters is the list of news from News_search
+    :return: Returns in a paragraph the current market information, what drives the market behaviour and what
+    is more relevant.
+    """
+
+def market_data_morning():
+    """
+    This function evaluates the market position with a 15 minutes delay (similar as other options, even as
+    other payment methods. The information that analyse include index, stocks and commodities.
+    :return: With the information from the market the function elaborate two paragraphs that summarize the
+    most relevant market movements
+    """
